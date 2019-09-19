4bfa0e4ef29794e82e7f7850195a0a4e69733524
ranjit-das <Ranjit.Das@ons.gov.uk>
2019-09-19 12:08:40 +0100

    LU-3001: take on db export into s3

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# takeon-validationdb-export-lambda
+# takeon-db-export-lambda
+This repository is used to do the following -
+  1. access Business layer's GraphQL Endpoint that returns the whole database as Json String
+  2. Saves the Json String of Step 1 into a file in S3 bucket
+
+## Dependency
+The business layer's Load Balancer should be up and running. Also it should allow access to NAT's public IP so that this Lambda can access the endpoint.
+
+## Environment variables in Lamda
+  GRAPHQL_ENDPOINT
+  S3_BUCKET
+
+## Lambda URL
+  https://eu-west-2.console.aws.amazon.com/lambda/home?region=eu-west-2#/functions/takeon-validationdb-export-lambda-dev-main?tab=graph
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
@@ -24,28 +25,34 @@ func main() {
 
 func handle() {
 	fmt.Println("Starting the application...")
+
+	cdbExport := make(chan string)
+	go callGraphqlEndpoint(cdbExport)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go saveToS3(cdbExport, &wg)
+	wg.Wait()
+}
+
+func callGraphqlEndpoint(cdbExport chan string) {
 	var gqlEndpoint = os.Getenv("GRAPHQL_ENDPOINT")
-	var dataExport = ""
+	fmt.Println("Going to access  Graphql Endpoint: ", gqlEndpoint)
 	response, err := http.Get(gqlEndpoint)
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 	} else {
 		data, _ := ioutil.ReadAll(response.Body)
-		fmt.Println(string(data))
-		dataExport = string(data)
-		// fmt.Println(response)
+		cdbExport <- string(data)
+		fmt.Println("Accessing Graphql Endpoint done\n")
 	}
+}
 
-	// cdbExport := make(chan string)
+func saveToS3(cdbExport chan string, waitGroup *sync.WaitGroup) {
 
-	// callGraphqlEndpoint(cdbExport)
-
-	// dbExport := <- cdbExport
-
+	dbExport := <-cdbExport
 	var bucketFilenamePrefix = "takeon-data-export-"
 
-	currentTime := time.Now().Format("2019-09-11-00:00:00")
-	// fmt.Println(t.Format("20060102150405"))
+	currentTime := time.Now().Format("2006-01-02-15:04:05")
 
 	fmt.Printf("Region: %q\n", region)
 	config := &aws.Config{
@@ -60,7 +67,8 @@ func handle() {
 	filename := strings.Join([]string{bucketFilenamePrefix, currentTime}, "")
 	fmt.Printf("Bucket filename: %q\n", filename)
 
-	reader := strings.NewReader(dataExport)
+	reader := strings.NewReader(string(dbExport))
+	var err error
 	_, err = uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(filename),
@@ -72,17 +80,6 @@ func handle() {
 	}
 
 	fmt.Printf("Successfully uploaded %q to s3 bucket %q\n", filename, bucket)
-}
+	waitGroup.Done()
 
-func callGraphqlEndpoint(cdbExport chan string) {
-	var gqlEndpoint = os.Getenv("GRAPHQL_ENDPOINT")
-	response, err := http.Get(gqlEndpoint)
-	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		fmt.Println(string(data))
-		cdbExport <- string(data)
-		// fmt.Println(response)
-	}
 }
