919b92baedfcd8eedfa5d5b0537c376131ab2339
sufyan <sufyaniqbal@me.com>
2020-10-08 15:47:56 +0100

    create terraform

---
+++ aws_infrastructure/db_export.tf
@@ -0,0 +1,71 @@
+# Set Provider as AWS and region
+provider "aws" {
+    region = var.region
+    version = "~> 2"
+}
+
+terraform {
+  backend "s3" {}
+}
+
+locals {
+  name_prefix = "${var.common_name_prefix}-validation-${var.environment_name}"
+  vpc_prefix = "${var.common_name_prefix}-${var.environment_name}"
+}
+
+
+# Importing existing resources for lambdas to use
+data "aws_security_group" "vpc-private" {
+  filter {
+    name = "tag:Name"
+    values = ["${local.vpc_prefix}-vpc-private"]
+  }
+}
+
+data "aws_subnet" "private-subnet" {
+  filter {
+    name = "tag:Name"
+    values = ["${local.vpc_prefix}-vpc-private-subnet"]
+  }
+}
+
+data "aws_subnet" "private-subnet2" {
+  filter {
+    name = "tag:Name"
+    values = ["${local.vpc_prefix}-vpc-private-subnet2"]
+  }
+}
+
+data "aws_lb" "business-layer-lb" {
+    name = "${local.vpc_prefix}-bl"
+}
+
+data "aws_caller_identity" "current" {}
+
+# input
+resource "aws_sqs_queue" "db-input" {
+  name = "${local.name_prefix}-db-input"
+  redrive_policy = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dlq.arn}\",\"maxReceiveCount\":3}"
+
+  tags = merge(
+    var.common_tags,
+    {
+    Name = "${local.name_prefix}-db-input",
+    "ons:name" = "${local.name_prefix}-db-input"
+    }
+  )
+}
+
+# output
+resource "aws_sqs_queue" "db-output" {
+  name = "${local.name_prefix}-db-input"
+  redrive_policy = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dlq.arn}\",\"maxReceiveCount\":3}"
+
+  tags = merge(
+    var.common_tags,
+    {
+    Name = "${local.name_prefix}-db-input",
+    "ons:name" = "${local.name_prefix}-db-input"
+    }
+  )
+}
